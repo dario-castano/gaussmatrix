e4dc44cdd30e060502a98e5b0c3190c3efcf1999
Dario Castaño <dario.castano@aim.com>
2024-03-21 16:37:32 -0500

    add some functionality

--- src/gaussmatrix/matrix.clj
+++ src/gaussmatrix/matrix.clj
@@ -18,15 +18,44 @@
   [matrix]
   (not= (cmx/det (cmx/reshape matrix (repeat 2 (cmx/row-count matrix)))) 0))
 
-(defn solve 
+(defn make-pivot 
+  "Make a row of a matrix a pivot turning the target position into a one" 
+  [matrix index]
+  (let [factor (cmx/mget matrix index index)]
+    (cmx/multiply-row matrix index (/ 1 factor))))
+
+(defn make-reduction
+  "Make a reduction subtracting the target row minus the product of the target
+   element times the pivot row"
+  [matrix row-index row-pivot]
+  (cmx/sub (cmx/get-row matrix row-index) 
+           (cmx/mul (cmx/get-row matrix row-pivot) (cmx/mget matrix row-index row-pivot))))
+
+(defn flip-row-signs
+  "Flip the signs of all elements in a row"
+  [matrix row]
+  (cmx/multiply-row matrix row -1))
+
+(defn rec-gauss "" [matrix row-index ] ())
+
+(defn run-gauss 
+  "" 
+  [matrix rows cols]
+  (if (or (empty? rows) (empty? cols)) 
+    matrix 
+    ((let)
+     
+    )))
+
+
+
+(defn solve
   "Solve the matrix using Gaussian reduction"
   [matrix]
   (let [right-shape (correct-shape? matrix)
         is-invertible (invertible? matrix)
         cols (range (cmx/row-count matrix))
-        rows (range (cmx/row-count matrix))
-        ]
+        rows (range (cmx/row-count matrix))]
     (when-not right-shape (throw (RuntimeException. "Matrix must be square")))
     (when-not is-invertible (throw (RuntimeException. "Matrix must be invertible")))
-    ))
-
+    (run-gauss matrix rows cols)))
