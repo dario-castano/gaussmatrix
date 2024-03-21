3a439e0dc31569f07ea189eee1ddc1baf36feac0
Dario Castaño <dario.castano@aim.com>
2024-03-21 07:29:52 -0500

    Add lsp and kondo to gitignore

--- src/gaussmatrix/core.clj
+++ src/gaussmatrix/core.clj
@@ -1,13 +1,19 @@
 (ns gaussmatrix.core
   (:gen-class))
 
+(defn square-matrix?
+  "Checks if the matrix has the same height and width"
+  [matrix]
+  (= (count matrix) (map count matrix)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [example-list [[1 -2 2 -3] 
                       [3 4 -1 1] 
                       [2 -3 2 -1] 
-                      [1 1 -3 -2]]]
+                      [1 1 -3 -2]]
+        square? (square-matrix? example-list)]
     
     ))
 
