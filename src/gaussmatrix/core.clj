ada2f3f19b5b11316b2f3049f08ff3a584bbe430
Dario Castaño <dario.castano@aim.com>
2024-03-21 07:28:06 -0500

    Add square matrix check

--- src/gaussmatrix/core.clj
+++ src/gaussmatrix/core.clj
@@ -4,4 +4,15 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [example-list [[1 -2 2 -3] 
+                      [3 4 -1 1] 
+                      [2 -3 2 -1] 
+                      [1 1 -3 -2]]]
+    
+    ))
+
+(defn square-matrix?
+  "Checks if the matrix has the same height and width" 
+  [matrix]
+  (= (count matrix) (map count matrix)))
+
